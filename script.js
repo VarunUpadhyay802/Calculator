35b161ab52e69a7813186ed358949be6ce4d98d4
Varun <varun.802.vu@gmail.com>
2023-01-20 23:55:13 +0530

    Digital Clock

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-function noice(){
-    console.log("noiccccceeeeeeeeeeeeee");
+const duration=new Date();
+console.log(duration);
+console.log(duration.getHours());
+console.log(duration.getMinutes());
+console.log(duration.getSeconds());
+
+const greet = document.getElementById("top");
+const mid=document.getElementById("mid")
+const hour=document.getElementById("hr")
+
+var x=duration.getSeconds();
+var y=duration.getMinutes();
+var z=duration.getHours();
+mid.innerHTML=y;
+greet.innerHTML=x;
+hour.innerHTML=z;
+function seconds() {
+    greet.innerHTML=x;
+  console.log(x);
+  x++;
+   if(x==60){
+    x=0;
+    mid.innerHTML=y;
+    y++;
+    if(y==60){
+        y=0;
+        hour.innerHTML=z
+        z++;
+        
+    }
+   }
 }
-setTimeout(noice,80000)
+var id=setInterval(seconds,1000)
+
+
+
+
+
+
+
